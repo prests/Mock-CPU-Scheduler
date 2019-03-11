7cfb4ae578e32afd4464855c20b9341f92c9dde1
Shayne Preston <prests@rpi.edu>
2019-03-11 17:25:56 -0400

    working exponential distribution

--- exp-random.py
+++ exp-random.py
@@ -1,16 +1,55 @@
+import rand48
+import math
 
-
-def main(r, l, upperBound):
+def classExample(seed, upperBound):
+    r = rand48.Rand48(0)
+    r.srand(seed)
     iterations = 1000000
     total = 0
+    minVal = 0
+    maxVal = 0
+    l = 0.001
     for i in range(0, iterations):
-        r.drand()
-        x = -log(r)/float(l)
+        uniformDist = r.drand()
+        x = -math.log(uniformDist)/float(l)
         if(x > upperBound):
             i -= 1
             continue
         total += x
+        if(i < 20):
+            print(x)
+        if(i == 0 or x < minVal):
+            minVal = x
+        if(i ==0 or x > maxVal):
+            maxVal = x
+    
+    avg = total/iterations
+
+    print("minimum value: %d", minVal)
+    print("maximum value: %d", maxVal)
+    print("average value: %d", avg)
+
+def expDist(uniformDist, l, upperBound, r):
+    while(True):
+        x = -math.log(uniformDist)/float(l)
+        if(x > upperBound):
+            uniformDist = r.drand()
+            print("failed")
+        else:
+            break
+    return x
+        
         
     avg = total/iterations
     print("x average: %s", avg)
     return avg
+
+
+if __name__ == "__main__":
+    seed = 80
+    upperBound = 3000
+    rand = rand48.Rand48(0)
+    rand.srand(seed)
+    classExample(seed, upperBound)
+    val = expDist(rand.drand(), 0.001, 10, rand)
+    print(val)
