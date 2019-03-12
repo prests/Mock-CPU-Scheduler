c22cfc78202c03e1735db71251d947170d114a3c
Shayne Preston <prests@rpi.edu>
2019-03-11 21:27:52 -0400

    comment on status

--- process.py
+++ process.py
@@ -7,12 +7,19 @@
         import process
 
         p = process.Process(arrivalTime, status, cpuBurstTime)
+
+        status:
+        0 - not arrived
+        1 - arrived
+        2 - running
+        3 - ready
+        4 - blocked
 '''
 class Process(object):
-    def __init__(self, arrive, status, cpuBurstNumber):
+    def __init__(self, arrive, status, cpuBurstNumber, cpuBurstTimes):
         self.arrivalTime = arrive
         self.state = status
         self.cpuBurstNum = cpuBurstNumber
-        self.cpuBurstTimes = []
+        self.cpuBurstTimes = cpuBurstTimes
     def addBurstTime(self, burstTime):
         cpuBurstTime.append(burstTime)
