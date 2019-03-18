461917d244d1f40e3d9d97cffc10d1ae50920cdb
Shayne Preston <prests@rpi.edu>
2019-03-17 20:01:41 -0400

    blah

--- sjf.py
+++ sjf.py
@@ -15,13 +15,16 @@ def main(processes, tCS, alpha, tau):
                     i.changeState(3)
                     queue.append(i)
                 else:
+                    nextTau = expAverage.nextTau(tau, alpha, queue[0].actualBurstTime[queue[0].completed-1]) #THIS ISN'T RIGHT??
+                    tau = nextTau
                     for j in range(1,len(queue)):
-                        nextTau = expAverage.nextTau(tau, alpha, ) #THIS ISN'T RIGHT
                         if(nextTau < queue[0].cpuBurstTimes[queue[0].completed]):
                             print("Process is jumping the line")
+                            queue.insert(j, i)
+                            i.changeState(3)
                         elif(j == len(queue)-1):
                             queue.append(i)
+                            i.changeState(i)
 
         if(len(queue) > 0):
-            
-
+            if
